27bb995f84b53a2c03820c64dddccd5181d787e5
GPJD <74803693+GPJD@users.noreply.github.com>
2021-07-08 20:18:40 -0500

    Update
    Penultimo ejercicio de diccionarios

--- Ejercicios.py
+++ Ejercicios.py
@@ -425,3 +425,43 @@ while continuar:
             deuda=deuda-deuda
     continuar=input("Volver al menu? Si/No ")=="Si"
 #Fin del ejercicio
+#Simular base de datos donde se almacene datos de clientes que contengan un numero de identificacion como llave
+#El programa permite Añadir,Eliminar,MostrarClientes,ListarClientes,ListrarClientesVip,Salir
+clientes={}
+opcion=''
+while opcion !='6':
+    if opcion =='1':
+       nif=input("Introduce Numero de identificacion del cliente: ") 
+       nombre=input("Introduce el nombre del cliente: ")
+       direccion=input("Introduce la direccion: ")
+       telefono=input("Coloca el telefono: ")
+       correo=input("Ingresa el correo electronico: ")
+       vip=input("¿El cliente es vip? S/N ")
+       cliente={'nombre':nombre,'direccion':direccion,'telefono':telefono,'correo':correo,'vip':vip=='S'}
+       clientes[nif]=cliente
+    if opcion =='2':
+        nif=input("Ingresa el Numero de identificacion del cliente: ")
+        if nif in clientes:
+            del clientes[nif]
+        else:
+            print("No existe cliente con el numero de identificacion: ",nif)
+    if opcion=='3':
+        nif=input("Ingresa el numero de identificacion del cliente: ")
+        if nif in clientes:
+            print('nif:',nif)
+            for clave,valor in clientes[nif].items():
+                print(clave.title()+':',valor)
+        else:
+            print("No existe el cliente con el numero de identificacion: ",nif)
+    if opcion=='4':
+        print("Lista de clientes: ")
+        for clave,valor in clientes.items():
+            print(clave,valor['nombre'])
+    if opcion=='5':
+        print("Lista de clientes Vip")
+        for clave,valor in clientes.items():
+            if valor['vip']:
+                print(clave,valor['nombre'])
+    opcion=input("Menu de opciones:\n (1)Añadir cliente\n(2)Eliminar Cliente\n(3)Mostrar Cliente\n(4)Listar Clientes\n(5)Listar Clientes Vip\n(6)Terminar ")
+    #Fin del ejercicio
+    
